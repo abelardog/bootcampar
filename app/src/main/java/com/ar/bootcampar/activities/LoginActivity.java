847b7b375979633910eabf9f0e6a1831a28e317e
Roberto <rpgrca@gmail.com>
2023-09-23 17:24:17 -0300

    add simple login handler with toast

--- app/src/main/java/com/ar/bootcampar/activities/LoginActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/LoginActivity.java
@@ -2,11 +2,16 @@ package com.ar.bootcampar.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.ar.bootcampar.R;
+
+import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,15 +19,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView tvError = (TextView) findViewById(R.id.tvError);
-        tvError.setText("Error");
     }
 
     public void onClickRegister(){
-        // Crea el intento de la nueva actividad
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-        // Abre la nueva actividad
         startActivity(intent);
+    }
 
+    public void onLoginClick(View view) {
+        TextView usernameTextView = (TextView)findViewById(R.id.editEmailAddress);
+        if (usernameTextView.getText() == "alumno@gmail.com") {
+            TextView passwordTextView = (TextView)findViewById(R.id.editTextPassword);
+            if (passwordTextView.getText() == "123456") {
+                // TODO: Pasar a la actividad Home
+                Toast.makeText(getApplicationContext(), "Ingreso exitoso", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
     }
 }
