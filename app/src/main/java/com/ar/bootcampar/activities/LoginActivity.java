b548bea70e9de039deb4db8fb9ffcd9ea662aef8
Roberto <rpgrca@gmail.com>
2023-09-24 00:54:05 -0300

    redirect to home from login

--- app/src/main/java/com/ar/bootcampar/activities/LoginActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/LoginActivity.java
@@ -31,12 +31,17 @@ public class LoginActivity extends AppCompatActivity {
         if (usernameTextView.getText().toString().equals("alumno@gmail.com")) {
             TextView passwordTextView = (TextView)findViewById(R.id.editTextPassword);
             if (passwordTextView.getText().toString().equals("123456")) {
-                // TODO: Pasar a la actividad Home
                 Toast.makeText(getApplicationContext(), R.string.welcomeMessage, Toast.LENGTH_SHORT).show();
-                return;
+
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), R.string.invalidLoginMessage, Toast.LENGTH_LONG).show();
             }
         }
-
-        Toast.makeText(getApplicationContext(), R.string.invalidLoginMessage, Toast.LENGTH_LONG).show();
+        else {
+            Toast.makeText(getApplicationContext(), R.string.invalidLoginMessage, Toast.LENGTH_LONG).show();
+        }
     }
 }
