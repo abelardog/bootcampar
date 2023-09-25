35ab30d5c6450aef155c213b71236e7f35fe008c
Roberto <rpgrca@gmail.com>
2023-09-25 10:10:25 -0300

    add simple registration routines

--- app/src/main/java/com/ar/bootcampar/activities/RegisterActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/RegisterActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ar.bootcampar.R;
 
@@ -22,6 +24,24 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onRegisterClick(View view){
+        String firstname = ((EditText)findViewById(R.id.editFirstName)).getText().toString();
+        String lastname = ((EditText)findViewById(R.id.editLastName)).getText().toString();
+        String email = ((EditText)findViewById(R.id.editEmailAddress)).getText().toString();
+        String password = ((EditText)findViewById(R.id.editPassword)).getText().toString();
+        String confirmPassword = ((EditText)findViewById(R.id.editConfirmPassword)).getText().toString();
 
+        if (! firstname.isEmpty() && ! lastname.isEmpty() && ResetPasswordActivity.isValidEmail(email) && !password.isEmpty() && !confirmPassword.isEmpty()) {
+            if (password.equals(confirmPassword)) {
+                Toast.makeText(this, R.string.registration_success_message, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(this, R.string.password_dont_match_message, Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            Toast.makeText(this, R.string.please_complete_data_message, Toast.LENGTH_SHORT).show();
+        }
     }
 }
