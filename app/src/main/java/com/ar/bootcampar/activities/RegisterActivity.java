55d600d9fa5a91260bb9358a621f0c8d203cea23
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2023-09-24 23:09:58 -0300

    Crea metodo para volver a ir a login
    Agrega tambien metodo vacío del botón Registrarse

--- app/src/main/java/com/ar/bootcampar/activities/RegisterActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.ar.bootcampar.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ar.bootcampar.R;
 
@@ -12,5 +14,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
+    }
+
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onRegisterClick(View view){
+
     }
 }
