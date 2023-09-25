0fc66d55fcb1909ba97444be59c9c9ef2578bdfa
Roberto <rpgrca@gmail.com>
2023-09-24 22:31:34 -0300

    add activity for reset password

---
+++ app/src/main/java/com/ar/bootcampar/activities/ResetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.ar.bootcampar.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ar.bootcampar.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
