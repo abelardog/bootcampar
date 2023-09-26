033e6d0e264b988961b1da8faa6752f656e33bba
Roberto <rpgrca@gmail.com>
2023-09-25 23:37:01 -0300

    add navigation to edit profile from profile

--- app/src/main/java/com/ar/bootcampar/activities/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/ar/bootcampar/activities/ui/profile/ProfileFragment.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.ar.bootcampar.R;
 import com.ar.bootcampar.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
@@ -24,8 +26,16 @@ public class ProfileFragment extends Fragment {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button button = (Button) root.findViewById(R.id.gotoEditProfile);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_notifications_to_to_navigation_profile_edition);
+            }
+        });
+
         return root;
     }
 
