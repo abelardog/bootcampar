55d4a16880c77ab170e65442e71e7062f0dc4f2c
Roberto <rpgrca@gmail.com>
2023-09-27 22:21:10 -0300

    do not show enroll button when not logged in

--- app/src/main/java/com/ar/bootcampar/activities/CourseDetailActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/CourseDetailActivity.java
@@ -2,8 +2,12 @@ package com.ar.bootcampar.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ar.bootcampar.R;
 
@@ -19,5 +23,22 @@ public class CourseDetailActivity extends AppCompatActivity {
 
         TextView detailTextView = findViewById(R.id.detailTextView);
         detailTextView.setText(selectedItemTitle);
+
+        Intent intent = getIntent();
+        boolean loggedIn = intent.getBooleanExtra("loggedIn", false);
+
+        Button enroll = (Button) findViewById(R.id.detailEnrollBtn);
+        if (loggedIn) {
+            enroll.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(CourseDetailActivity.this, R.string.enrollment_success, Toast.LENGTH_SHORT).show();
+                    // TODO: Levantar actividad con listado de videos del curso
+                }
+            });
+        }
+        else {
+            enroll.setVisibility(View.INVISIBLE);
+        }
     }
 }
