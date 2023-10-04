b8a567e9cc68a4e17a2ed1a28754ed067c015cf4
Roberto <rpgrca@gmail.com>
2023-10-04 00:57:59 -0300

    use context to get messages, add logic for registration

--- app/src/main/java/com/ar/bootcampar/model/DatabaseManager.java
+++ app/src/main/java/com/ar/bootcampar/model/DatabaseManager.java
@@ -1,28 +1,45 @@
 package com.ar.bootcampar.model;
 
 import android.content.Context;
+import android.util.Pair;
+import android.widget.Toast;
+
+import com.ar.bootcampar.R;
+import com.ar.bootcampar.activities.ResetPasswordActivity;
 
 public class DatabaseManager {
+    private Context context;
     private IDatabase database;
 
-    public DatabaseManager(Context applicationContext) {
-        database = Database.CreateWith(applicationContext);
+    public DatabaseManager(Context context) {
+        this.context = context;
+        database = Database.CreateWith(context);
     }
 
-    public Usuario registrarUsuario(String nombre, String apellido, String email, String clave, Rol rol, String telefono, String invitacion) {
-        Grupo grupo = database.buscarGrupoONada(invitacion);
-        if (grupo == null) {
-            throw new RuntimeException(String.format("La invitacion %s es inválida.", invitacion));
+    public Pair<Usuario, String> registrarUsuario(String nombre, String apellido, String email, String clave, String confirmarClave, Rol rol, String telefono, String invitacion) {
+        if (! nombre.isEmpty() && ! apellido.isEmpty() && ResetPasswordActivity.isValidEmail(email) && !clave.isEmpty() && !confirmarClave.isEmpty() && !invitacion.isEmpty()) {
+            if (clave.equals(confirmarClave)) {
+                Grupo grupo = database.buscarGrupoONada(invitacion);
+                if (grupo == null) {
+                    return Pair.create(null, String.format("La invitacion %s es inválida.", invitacion));
+                }
+
+                Usuario usuario = database.buscarUsuarioONada(email);
+                if (usuario != null) {
+                    return Pair.create(null, String.format("Ya existe un usuario registrado con el e-mail %s", email));
+                }
+
+                usuario = database.crearUsuario(nombre, apellido, email, clave, rol, telefono);
+                Division division = database.crearDivision(usuario, grupo);
+
+                return Pair.create(usuario, context.getString(R.string.registration_success_message));
+            }
+            else {
+                return Pair.create(null, context.getString(R.string.password_dont_match_message));
+            }
         }
-
-        Usuario usuario = database.buscarUsuarioONada(email);
-        if (usuario != null) {
-            throw new RuntimeException(String.format("Ya existe un usuario registrado con el e-mail %s", email));
+        else {
+            return Pair.create(null, context.getString(R.string.please_complete_data_message));
         }
-
-        usuario = database.crearUsuario(nombre, apellido, email, clave, rol, telefono);
-        Division division = database.crearDivision(usuario, grupo);
-
-        return usuario;
     }
 }
