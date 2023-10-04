435b5bfd5e40945634d6415e9c2efafd5c06010a
Roberto <rpgrca@gmail.com>
2023-10-04 00:58:26 -0300

    add note for future

--- app/src/main/java/com/ar/bootcampar/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/ResetPasswordActivity.java
@@ -28,6 +28,7 @@ public class ResetPasswordActivity extends AppCompatActivity {
         }
     }
 
+    // TODO: Mover a clase logica
     public static boolean isValidEmail(CharSequence target) {
         return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
