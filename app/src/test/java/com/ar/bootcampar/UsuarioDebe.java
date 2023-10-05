693e08012c5395e7ab63bcb56b5da09ec3ef7907
Roberto <rpgrca@gmail.com>
2023-10-05 10:37:30 -0300

    move Usuario test constants to own file

--- app/src/test/java/com/ar/bootcampar/UsuarioDebe.java
+++ app/src/test/java/com/ar/bootcampar/UsuarioDebe.java
@@ -1,6 +1,6 @@
 package com.ar.bootcampar;
 
-import com.ar.bootcampar.model.Rol;
+import static com.ar.bootcampar.Constants.*;
 import com.ar.bootcampar.model.Usuario;
 
 import org.junit.Test;
@@ -14,14 +14,6 @@ import static org.junit.Assert.*;
 
 @RunWith(Theories.class)
 public class UsuarioDebe {
-    public static final int ID = 1;
-    public static final String NOMBRE = "Luis";
-    public static final String APELLIDO = "Agote";
-    public static final String EMAIL = "luis.agote@gmail.com";
-    public static final String CLAVE = "123456";
-    public static final Rol ROL = Rol.Estudiante;
-    public static final String TELEFONO = "1234-5678";
-
     @Test
     public void serCreado_cuandoLosDatosSonCorrectos() {
         Usuario sut = new Usuario(ID, NOMBRE, APELLIDO, EMAIL, CLAVE, ROL, TELEFONO);
