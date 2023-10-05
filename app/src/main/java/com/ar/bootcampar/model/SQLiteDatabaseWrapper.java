b4125aeef380765dce2b734f0c8494b1b263fc36
Roberto <rpgrca@gmail.com>
2023-10-04 23:14:44 -0300

    implement SQLiteDatabase wrapper

---
+++ app/src/main/java/com/ar/bootcampar/model/SQLiteDatabaseWrapper.java
@@ -0,0 +1,53 @@
+package com.ar.bootcampar.model;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class SQLiteDatabaseWrapper implements ISQLiteDatabaseWrapper {
+    private SQLiteDatabase sqliteDatabase;
+
+    public SQLiteDatabaseWrapper(SQLiteDatabase sqliteDatabase) {
+        this.sqliteDatabase = sqliteDatabase;
+    }
+
+    @Override
+    public void close() {
+        this.sqliteDatabase.close();
+    }
+
+    @Override
+    public Cursor query(String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy) {
+        return this.sqliteDatabase.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+    }
+
+    @Override
+    public void endTransaction() {
+        this.sqliteDatabase.endTransaction();
+    }
+
+    @Override
+    public void setTransactionSuccessful() {
+        this.sqliteDatabase.setTransactionSuccessful();
+    }
+
+    @Override
+    public void beginTransaction() {
+        this.sqliteDatabase.beginTransaction();
+    }
+
+    @Override
+    public long insert(String table, String nullColumnHack, ContentValues values) {
+        return this.sqliteDatabase.insert(table, nullColumnHack, values);
+    }
+
+    @Override
+    public int delete(String table, String whereClause, String[] whereArgs) {
+        return this.sqliteDatabase.delete(table, whereClause, whereArgs);
+    }
+
+    @Override
+    public int update(String table, ContentValues values, String whereClause, String[] whereArgs) {
+        return this.sqliteDatabase.update(table, values, whereClause, whereArgs);
+    }
+}
