9b92ba17d5ed32eb360ef7f33ce653f667efe94f
Roberto <rpgrca@gmail.com>
2023-10-05 22:28:18 -0300

    add test, should send correct user id

---
+++ app/src/test/java/com/ar/bootcampar/BorrarUsuarioDebe.java
@@ -0,0 +1,36 @@
+package com.ar.bootcampar;
+
+import static com.ar.bootcampar.support.Constants.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import com.ar.bootcampar.model.Database;
+import com.ar.bootcampar.model.ISQLiteDatabaseWrapper;
+import com.ar.bootcampar.model.Usuario;
+import com.ar.bootcampar.support.SqliteDatabaseWrapperSpy;
+import com.ar.bootcampar.support.TestableDatabase;
+
+import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.FromDataPoints;
+import org.junit.experimental.theories.Theory;
+
+public class BorrarUsuarioDebe {
+    @Test
+    public void recibirElIdParaBorrar() {
+        Usuario usuario = crearUsuarioDePrueba();
+        SqliteDatabaseWrapperSpy spy = new SqliteDatabaseWrapperSpy.Builder()
+                .conDeleteRetornando(1)
+                .build();
+
+        Database sut = new TestableDatabase(spy);
+        sut.borrarUsuario(usuario);
+
+        assertEquals(String.valueOf(ID), spy.getWhereArgs()[0]);
+    }
+
+    private static Usuario crearUsuarioDePrueba() {
+        return new Usuario(ID, NOMBRE, APELLIDO, EMAIL, CLAVE, ROL, TELEFONO);
+    }
+}
