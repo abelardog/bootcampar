50fdc912551bc90ab9ee8db3902eb0cd6a107f07
Roberto <rpgrca@gmail.com>
2023-10-07 10:35:10 -0300

    add constants to test Leccion

--- app/src/test/java/com/ar/bootcampar/support/Constants.java
+++ app/src/test/java/com/ar/bootcampar/support/Constants.java
@@ -10,4 +10,14 @@ public class Constants {
     public static final String CLAVE = "123456";
     public static final Rol ROL = Rol.Estudiante;
     public static final String TELEFONO = "1234-5678";
+
+    public static final String TITULO_LECCION = "Introducción a JavaScript";
+    public static final String CONTENIDO_LECCION = "https://www.youtube.com/video/xyz";
+    public static final int DURACION_LECCION = 10;
+    public static final int ORDEN_LECCION = 1;
+
+    public static final String IMAGEN_CURSO = "https://www.imgur.com/xyz";
+    public static final String TITULO_CURSO = "Maestría en JavaScript";
+    public static final String DESCRIPCION_CURSO = "Con este curso sabrá programar en JavaScript desde un carrito de compras a un e-commerce!";
+    public static final boolean ES_FAVORITO_CURSO = true;
 }
