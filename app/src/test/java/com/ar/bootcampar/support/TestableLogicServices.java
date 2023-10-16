37b8d57e3b67275361076ba94d3140f329577325
Roberto <rpgrca@gmail.com>
2023-10-15 22:24:09 -0300

    add test, check for invalid input in registering

--- app/src/test/java/com/ar/bootcampar/support/TestableLogicServices.java
+++ app/src/test/java/com/ar/bootcampar/support/TestableLogicServices.java
@@ -1,10 +1,15 @@
 package com.ar.bootcampar.support;
 
+import com.ar.bootcampar.model.IDatabase;
 import com.ar.bootcampar.model.LogicServices;
 
 public class TestableLogicServices extends LogicServices {
     public TestableLogicServices() {
         super(null, null);
+    }
+
+    public TestableLogicServices(IDatabase database) {
+        super(null, database);
     }
 
     @Override
