63d0411999652a981a97ca9d7c9aff6266dae6bc
Roberto <rpgrca@gmail.com>
2023-10-18 00:44:52 -0300

    initialize recycler view with lessons from course

--- app/src/main/java/com/ar/bootcampar/activities/VideoListActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/VideoListActivity.java
@@ -7,11 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ar.bootcampar.R;
+import com.ar.bootcampar.databinding.ActivityCourseListBinding;
+import com.ar.bootcampar.model.Curso;
 import com.ar.bootcampar.model.Inscripcion;
+import com.ar.bootcampar.model.Leccion;
+import com.ar.bootcampar.model.LogicServices;
+import com.ar.bootcampar.services.CourseAdapter;
+import com.ar.bootcampar.services.LeccionAdapter;
+
+import java.util.List;
 
 public class VideoListActivity extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityCourseListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,14 @@ public class VideoListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video_list);
 
         Inscripcion inscripcion = (Inscripcion)getIntent().getSerializableExtra(INSCRIPTION_FOR_VIDEO_LIST);
+
+        LogicServices logicServices = new LogicServices(getApplicationContext());
+        List<Leccion> listaLecciones = logicServices.buscarLecciones(inscripcion.getCurso());
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewLessons);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LeccionAdapter adapter = new LeccionAdapter(listaLecciones);
+        recyclerView.setAdapter(adapter);
     }
 
     public void onChapterClick(View view) {
