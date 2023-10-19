57187eae39186f541391228d0aecb4efe6a046a1
Roberto <rpgrca@gmail.com>
2023-10-19 11:10:45 -0300

    add webkit dependency for yt player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.webkit:webkit:1.4.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
