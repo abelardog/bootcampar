3cfebe712496a6513b111a5b6f893dab4b8b4c90
Roberto <rpgrca@gmail.com>
2023-10-19 13:19:49 -0300

    play video when accessing video view

--- app/src/main/java/com/ar/bootcampar/activities/CourseVideoActivity.java
+++ app/src/main/java/com/ar/bootcampar/activities/CourseVideoActivity.java
@@ -3,13 +3,14 @@ package com.ar.bootcampar.activities;
 import static com.ar.bootcampar.model.utilities.IntentConstants.LESSON_FOR_COURSE;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ar.bootcampar.R;
 import com.ar.bootcampar.model.Leccion;
-import com.ar.bootcampar.model.LogicServices;
 
 public class CourseVideoActivity extends AppCompatActivity {
 
@@ -20,9 +21,14 @@ public class CourseVideoActivity extends AppCompatActivity {
 
         Leccion leccion = (Leccion)getIntent().getSerializableExtra(LESSON_FOR_COURSE);
 
-        LogicServices logicServices = new LogicServices(getApplicationContext());
         ((TextView)findViewById(R.id.textLessonPlayerTitle)).setText(leccion.getTitulo());
         ((TextView)findViewById(R.id.textLessonPlayerLesson)).setText(String.format("Lección %d", leccion.getOrden()));
         ((TextView)findViewById(R.id.textLessonPlayerContents)).setText(leccion.getContenido());
+
+        WebView webView = findViewById(R.id.videoClass);
+        String video = String.format("<iframe width=\"100%%\" src=\"%s\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>", leccion.getVinculo());
+        webView.loadData(video, "text/html", "utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
     }
 }
